1fad93b65bdbe9cbfd8f1f7133a4e4f89871ca2a
ynesta <camille.ynesta@gmail.com>
2018-03-19 23:52:37 +0100

    E1

---
+++ EE1Seance5/src/E1/E1.java
@@ -0,0 +1,14 @@
+package E1;
+
+import java.util.Scanner;
+import java.util.*;
+
+public class E1 {
+
+	Scanner sc = new Scanner(System.in);
+	int i = 1;
+	
+    while (i != 0){
+    	int i = sc.nextInt();
+    }
+}
